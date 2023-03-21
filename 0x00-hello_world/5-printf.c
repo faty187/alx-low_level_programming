478c93e01da2897dec747a78ccd274eb5f7dc6d7
faty187 <fatimaezzahraaelhamday0@gmail.com>
2023-03-20 20:13:07 -0700

    commit hello word

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * c program that print using printf function 
+ *
+ * returns 0 in success
+ */
+int main (void)
+ {
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
+ }	
