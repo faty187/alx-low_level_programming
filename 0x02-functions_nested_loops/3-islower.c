c723af36449306fabf976409e6da26b02c7b4a92
faty187 <fatimaezzahraaelhamday0@gmail.com>
2023-03-22 18:05:25 -0700

    c fiun,tipons commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _is lower - functions to check if 
+ *             charachter is lowercase
+ *
+ * 0c: checks input of function
+ *
+ * Return: returns 1 if 'c' is lowercase
+ *                otherwise always 0 (success)
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return(1)
+			return(0);
+}
