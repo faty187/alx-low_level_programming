0de2e9346c3bd72d74e6586e4ba614f24a7af831
faty187 <fatimaezzahraaelhamday0@gmail.com>
2023-03-23 10:29:03 -0700

    debugging commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ *
+ * 0c: takes number input
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
+}
