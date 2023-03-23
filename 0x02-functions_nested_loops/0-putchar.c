6582aaa0054b0ba7a315065a781ab14c47bcd701
faty187 <fatimaezzahraaelhamday0@gmail.com>
2023-03-22 17:47:39 -0700

    c functions commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /** 
  * main - Entry point
  *
@@ -8,13 +7,13 @@
  * Return: Always 0 (success )
  */
 int main(void)
-	{
+{
 		char str[] = "_putchar";
 		int ch;
 
 		for (ch = 0; ch < 8; ch++)
 			_putchar(str[ch]);
 		_putchar('\n');
-		
+
 		return (0);
-	}
+}
