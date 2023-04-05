64228bd40382969562fadc5cbd3bf76227cedef1
faty187 <fatimaezzahraaelhamday0@gmail.com>
2023-04-05 15:10:31 -0700

    commit of the tasks of the 8th projects ; the recusion in c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
-/*
+/**
  * _puts_recursion - print a string
  * @s:string
  * Return:void
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-	printf("%c", *s);
+if (*s)
+{
+	_putchar(*s);
 	_puts_recursion(s + 1);
-	}
-	else
-	{
-		 printf("\n");
-	}
+}
+else
+{
+	_putchar('\n');
+}
+
 }
