e643c2482e981a91dcbca39d4acca06db506a844
faty187 <fatimaezzahraaelhamday0@gmail.com>
2023-04-12 09:00:01 -0700

    the first commit of the first task of malloc and free project

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * create_array - Creates an array of characters
+ * and initializes it with a specific character.
+ * @size: The size of the array to be created.
+ * @c: The character to initialize the array with.
+ *
+ * Return: If size is zero or if memory allocation fails - NULL.
+ *         Otherwise - a pointer to the array.
+ */
+char *create_array(unsigned int size, char c);
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *arr = malloc(size * sizeof(char));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	arr[0] = c;
+	for (unsigned int i = 1; i < size; i++)
+	{
+		arr[i] = '\0';
+	}
+	return (arr);
+}
