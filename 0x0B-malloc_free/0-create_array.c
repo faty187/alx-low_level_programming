f1f442339df648bdd4647cfd66139d06087504c5
faty187 <fatimaezzahraaelhamday0@gmail.com>
2023-04-12 09:21:07 -0700

    malloc and free commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,13 +5,16 @@
 /*
  * create_array - Creates an array of characters
  * and initializes it with a specific character.
+ * main - takes two arguments and use the function create_array
+ *
  * @size: The size of the array to be created.
  * @c: The character to initialize the array with.
  *
  * Return: If size is zero or if memory allocation fails - NULL.
  *         Otherwise - a pointer to the array.
  */
-char *create_array(unsigned int size, char c);
+
+char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
 	{
@@ -30,4 +33,22 @@ char *create_array(unsigned int size, char c);
 		arr[i] = '\0';
 	}
 	return (arr);
+}
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s <size> <character>\n", argv[0]);
+		return (1);
+	}
+
+	int size = atoi(argv[1]);
+
+	char c = argv[2][0];
+
+	char *arr = create_array(size, c);
+
+	printf("%s\n", arr);
+	free(arr);
+	return (0);
 }
